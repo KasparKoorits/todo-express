1d748840dec283cf9f892d43298ec2c637b2ba64
KasparKoorits <kaspar.koorits@voco.ee>
2024-09-16 11:29:13 +0300

    Write data into file

--- index.js
+++ index.js
@@ -2,24 +2,47 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-//use ejs files to prepare template for views
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./tasks", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const tasks = data.split("\n");
+      resolve(tasks);
+    });
+  });
+};
+
 app.get("/", (req, res) => {
-  // get data from file
-  fs.readFile("./tasks", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-    console.log(typeof data);
-    console.log(data.split("\n"));
-    // Task list data
-    const tasks = ["Study HTML", "Study CSS", "Study JS", "Study OOP"];
+  readFile("./tasks").then((tasks) => {
+    console.log(tasks);
     res.render("index", { tasks: tasks });
+  });
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  //tasks list data from file
+  readFile("./tasks").then((tasks) => {
+    // add form sent task to array
+    tasks.push(req.body.task);
+    const data = tasks.join("\n");
+    fs.writeFile("./tasks", data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // redirect to see result
+      res.redirect("/");
+    });
   });
 });
 
